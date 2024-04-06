e5d9689e8f4163a920a32f820311a29b03135ba7
ashabb <ashishgole99955@gmail.com>
2024-04-06 21:52:59 +0530

    signout added

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -110,7 +110,38 @@ const signin = asyncHandler(async (req, res) => {
 
 })
 
+const signout = asyncHandler(async (req, res) => {
+    const user = await User.findByIdAndUpdate(
+        req.user._id,
+        {
+            $set: {
+                refreshToken: undefined
+            }
+        },
+        {
+            new: true
+        }
+    )
+
+    const options = {
+        httpOnly: true,
+        secure: true
+    }
+
+    return res
+        .status(200)
+        .clearCookie("accessToken", options)
+        .clearCookie("refreshToken", options)
+        .json(
+            new ApiResponse(
+                200,
+                {},
+                "user logged out"
+            )
+        )
+})
 export {
     signup,
-    signin
+    signin,
+    signout
 }
--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -1,9 +1,13 @@
 import { Router } from 'express'
-import { signin, signup } from '../controllers/user.controller.js'
+import { signin, signout, signup } from '../controllers/user.controller.js'
+import { verifyJWT } from '../middlewares/auth.middleware.js'
 
 const router = Router()
 
 router.route('/signup').post(signup)
 router.route('/signin').post(signin)
+
+// secured routes
+router.route("/signout").post(verifyJWT,signout)
 
 export default router
